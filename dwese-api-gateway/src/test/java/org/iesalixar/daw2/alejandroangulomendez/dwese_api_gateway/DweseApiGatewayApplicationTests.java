9703d53ad284559a9f841ba8344f3d0a59aec8c2
Alejandro241001 <alejandroangulomendez@outlook.com>
2025-06-12 11:05:16 +0200

    Primer commit

---
+++ dwese-api-gateway/src/test/java/org/iesalixar/daw2/alejandroangulomendez/dwese_api_gateway/DweseApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.alejandroangulomendez.dwese_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DweseApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
