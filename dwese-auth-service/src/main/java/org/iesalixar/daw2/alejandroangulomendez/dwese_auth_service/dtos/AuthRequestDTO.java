c0a840c092f76c1a99edf30d60985f6d3ae319b3
Alejandro241001 <alejandroangulomendez@outlook.com>
2025-06-12 12:43:32 +0200

    Falta AuthService

---
+++ dwese-auth-service/src/main/java/org/iesalixar/daw2/alejandroangulomendez/dwese_auth_service/dtos/AuthRequestDTO.java
@@ -0,0 +1,10 @@
+package org.iesalixar.daw2.alejandroangulomendez.dwese_auth_service.dtos;
+
+
+import lombok.Getter;
+
+@Getter
+public class AuthRequestDTO {
+    private String username;
+    private String password;
+}
